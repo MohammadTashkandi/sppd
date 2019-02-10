3300606e437d28516171d9aefa9ee4a13c83e7e4
Majed <majed_official@hotmail.com>
2019-02-10 23:31:56 +0300

    commit test by majeddd

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -8,7 +8,7 @@ export default class Index extends React.Component {
     render() {
         return (
             <div>
-                push test
+                push testtt 
                 <Header />
                 <SideBar />
             </div>
